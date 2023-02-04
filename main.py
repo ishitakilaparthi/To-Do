784876b9b6bd83d8ef5491675bc83c79b8f7c3da
ishitakilaparthi <ishita.kilaparthi@gmail.com>
2023-02-04 13:46:40 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from fastapi import FastAPI, status
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine("sqlite:///data.db")
-
-Base = declarative_base()
-
-class ToDo(Base):
-    __tablename__ = 'todos'
-    id = Column(Integer, primary_key=True)
-    task = Column(String(256))
+from database import Base, engine
 
 Base.metadata.create_all(engine)
 
