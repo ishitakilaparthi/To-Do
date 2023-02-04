98c63a56341974b1083871e01a7deb6c10b6e4a3
ishitakilaparthi <ishita.kilaparthi@gmail.com>
2023-02-04 13:45:01 +0530

    created database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine("sqlite:///data.db")
+
+Base = declarative_base()
+
+class ToDo(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer, primary_key=True)
+    task = Column(String(256))
